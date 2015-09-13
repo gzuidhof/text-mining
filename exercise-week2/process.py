0f0a55834fb23ba9efa0a3aa89b6a755fc86ea03
Guido <me@guido.io>
2015-09-13 04:28:57 +0200

    Page number/footnote detection

--- exercise-week2/process.py
+++ exercise-week2/process.py
@@ -6,8 +6,9 @@ def process_text(filename="input.txt"):
     file = open('input.txt','r')
     full_text = file.read().decode('utf8')
 
-
-    paged = re.sub(r'\n([0-9]+)\n',r'_NEWLINE_\1_NEWLINE_', full_text)
+    #Recognize multiple empty lines, used for page numbers and footnotes
+    #Place a flag there, to be added again later
+    paged = re.sub(r'\n\n',r'_NEWLINE_', full_text)
 
     #Hyphens at the end of sentences get removed, joining the two parts
     unhyphened = paged.replace('-\n',"")
@@ -19,8 +20,8 @@ def process_text(filename="input.txt"):
     #OPTIONAL WHITESPACE, NOT a dot pattern is present
     rebuilt = re.sub(r'[.][ ]([A-Z])([^?\s.])', r'.\n\1\2', collapsed)
     
+    #Re-add the multiple empty lines
     rebuilt = rebuilt.replace("_NEWLINE_","\n\n")
-    print rebuilt[:10000]    
     
     outfile = codecs.open('out.txt','w','utf-8')
     outfile.write(rebuilt)
