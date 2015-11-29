083f2c9e22117e2c699caa9579d5c5be9187f0e6
Guido <me@guido.io>
2015-11-29 23:12:21 +0100

    Faster filtering (multithreaded)

--- project/src/filter_and_lemma.py
+++ project/src/filter_and_lemma.py
@@ -1,13 +1,16 @@
+from __future__ import division
 import pandas as pd
 import glob
 import pickle
+import sys
 from collections import OrderedDict
+from multiprocessing.pool import ThreadPool
 
 INPUT_FOLDER = '../data/frogged'
 DATA_FOLDER = '../data'
 
 #PoS filter #http://ilk.uvt.nl/menno/files/docs/p_lrec_nlp4ugc12.pdf
-FILTER = ['LET','TW','LID','VG', 'VZ','SPEC(symb)']
+FILTER = tuple(['LET','TW','LID','VG', 'VZ','SPEC(symb)'])
 #LET (Leesteken aka Punctiation)
 #TW (Telwoord aka Number/ordinal)
 #LID (Lidwoord aka Determiner)
@@ -16,8 +19,7 @@ FILTER = ['LET','TW','LID','VG', 'VZ','SPEC(symb)']
 #SPEC(symb) (Symbool aka Symbol)
 
 def filter_dataframe(df):
-    for filter_term in FILTER:
-        df = df[~df['PoS'].str.startswith(filter_term)]
+    df = df[~df['PoS'].str.startswith(FILTER)]
     return df
 
 #Processes a single file
@@ -28,27 +30,36 @@ def process(filepath):
     df = df.drop(['PoSConfidence','Token','Morph'],axis=1)
     df = filter_dataframe(df)
 
+    #Join the lemmas together
     as_string = " ".join( [str(val) for val in list(df['Lemma'])])
+    #Lowercase
     lowercased = as_string.lower()
     return lowercased
 
-if __name__ == '__main__':
+def filter_and_lemma():
     files = glob.glob(INPUT_FOLDER+'/*.frog.out')
 
     lemmatized = {}
-    for i, filepath in enumerate(files):
-        file_id = filepath.split('\\')[-1].split('.')[0]
 
+    def process_file(filepath):
+        file_id = filepath.split('\\')[-1].split('.')[0]
         lemmatized[file_id] = process(filepath)
-        if i%100 == 0:
-            print i,'/',len(files)
+
+    pool = ThreadPool(processes=8)
+    num_tasks = len(files)
+    for i, _ in enumerate(pool.imap_unordered(process_file, files), 1):
+        sys.stderr.write('\rdone {0:%}'.format(i/num_tasks))
+    pool.close()
 
     #Order by key
     ordered = OrderedDict(sorted(lemmatized.items()))
 
-    for k,v in ordered.iteritems():
-        print k, v[:65]
+    #for k,v in ordered.iteritems():
+        #print k, v[:65]
 
     with open(DATA_FOLDER+'\\processed.p','w') as f:
         pickle.dump(ordered,f)
     print "Done!"
+
+if __name__ == '__main__':
+    filter_and_lemma()
