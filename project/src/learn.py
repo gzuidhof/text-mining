d1abf156eb28ec6ae2390c8a9f31164950b79bff
Guido <me@guido.io>
2015-11-30 01:06:32 +0100

    Create classifier for all labels

--- project/src/learn.py
+++ project/src/learn.py
@@ -10,28 +10,31 @@ from sklearn.pipeline import Pipeline
 def make_binary_y(y, val=0):
     return np.array([int(val in sample) for sample in y])
 
-
 if __name__ == '__main__':
-    #Load train data
-    X_train,y_train,filenames_train = dataset.load_train()
-    y_train_binary = make_binary_y(y_train) #True or False, 0,1
+    label_list = dataset.load_labels()
+    for x in range(25):
+        print ">  LABEL:", x, label_list[x]
+
+        #Load train data
+        X_train,y_train,filenames_train = dataset.load_train()
+        y_train_binary = make_binary_y(y_train,x) #True or False, 0,1
 
 
 
-    clf = Pipeline([('vect', CountVectorizer()),
-                          ('tfidf', TfidfTransformer()),
-                          ('clf', MultinomialNB()),
-     ])
+        clf = Pipeline([('vect', CountVectorizer()),
+                              ('tfidf', TfidfTransformer()),
+                              ('clf', MultinomialNB()),
+         ])
 
 
-    clf.fit(X_train, y_train_binary)
+        clf.fit(X_train, y_train_binary)
 
-    #Load test data
-    X_test,y_test,filenames_test = dataset.load_test()
-    y_test_binary = make_binary_y(y_test) #True or False, 0,1
+        #Load test data
+        X_test,y_test,filenames_test = dataset.load_test()
+        y_test_binary = make_binary_y(y_test,x) #True or False, 0,1
 
-    print "Actual:   ", y_test_binary
-    print "Predicted:", clf.predict(X_test)
-    print "Probabilities:", clf.predict_proba(X_test)
-    print "Accuracy:", clf.score(X_test, y_test_binary)
-    print "Mean label", 1-np.mean(y_test_binary), 1-np.mean(y_train_binary)
+        #print "Actual:   ", y_test_binary
+        #print "Predicted:", clf.predict(X_test)
+        #print "Probabilities:", clf.predict_proba(X_test)
+        print "Accuracy:", clf.score(X_test, y_test_binary)
+        print "Mean label", 1-np.mean(y_test_binary), 1-np.mean(y_train_binary)
