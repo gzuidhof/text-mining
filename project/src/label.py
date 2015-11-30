a7cccb846ff9dd5bc0435b3722c8c071a8d2a942
Guido <me@guido.io>
2015-11-30 01:06:12 +0100

    Print most common labels

--- project/src/label.py
+++ project/src/label.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     with open(DATA_FOLDER+'\\labels.p','r') as label_file:
         label_dict = pickle.load(label_file)
         labels = list(itertools.chain.from_iterable(label_dict.values()))
-        #print Counter(labels).most_common()
+        print Counter(labels).most_common()
         label_list = zip(*Counter(labels).most_common())[0]
 
         with open(DATA_FOLDER+'\\label_list.p','w') as f:
