675495bee64ac5d4079d56c43ef0d22db7c09a4b
Guido <me@guido.io>
2015-12-02 04:01:14 +0100

    Multilabel classifier and evaluation metrics

--- project/src/label.py
+++ project/src/label.py
@@ -8,8 +8,14 @@ DATA_FOLDER = '..\\data'
 if __name__ == '__main__':
     with open(DATA_FOLDER+'\\labels.p','r') as label_file:
         label_dict = pickle.load(label_file)
+
+        #Remove double entries
+        label_dict = {k:list(set(v)) for k,v in label_dict.iteritems()}
         labels = list(itertools.chain.from_iterable(label_dict.values()))
-        print Counter(labels).most_common()
+
+        for label in Counter(labels).most_common():
+		    print label
+
         label_list = zip(*Counter(labels).most_common())[0]
 
         with open(DATA_FOLDER+'\\label_list.p','w') as f:
@@ -23,3 +29,11 @@ if __name__ == '__main__':
 
         with open(DATA_FOLDER+'\\labels_int.p','w') as f:
             pickle.dump(ordered,f)
+
+        #Determine the frequency of 1, 2, 3 or more labels on individual documents
+        label_counts = [len(v) for v in label_dict.values()]
+        print Counter(label_counts)
+
+        for v in label_dict.values():
+            if len(v)>2:
+                print v
--- project/src/learn.py
+++ project/src/learn.py
@@ -8,33 +8,31 @@ from sklearn.pipeline import Pipeline
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import SGDClassifier
 
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.svm import SVC
+
+from sklearn.metrics import f1_score, accuracy_score, classification_report, zero_one_loss
+from sklearn.externals import joblib
 
 
-def make_binary_y(y, val=0):
+def binary_y(y, val=0):
     return np.array([int(val in sample) for sample in y])
 
-
-
-if __name__ == '__main__':
-
-    #Load data
+def multilabel_binary_y(y):
     label_list = dataset.load_labels()
 
-    X_train,y_train,filenames_train = dataset.load_train()
-    X_test,y_test,filenames_test = dataset.load_test()
+    mlb = MultiLabelBinarizer(classes=range(len(label_list)))
+    return mlb.fit_transform(y)
 
-    print "Train set", len(X_train), "Test set", len(X_test)
+def classifier_per_label(X_train,y_train,X_test,y_test):
 
     for x in range(25):
 
-        y_train_binary = make_binary_y(y_train,x) #0,1 for current label
-        y_test_binary = make_binary_y(y_test,x) #0,1 for current label
+        y_train_binary = binary_y(y_train,x) #0,1 for current label
+        y_test_binary = binary_y(y_test,x) #0,1 for current label
         print "----"
         print ">  LABEL", x, label_list[x], '('+str(1-np.mean(y_test_binary))+')'
-
-
-
-
 
         mnb = Pipeline([('vect', CountVectorizer()),
                               ('clf', MultinomialNB()),
@@ -60,7 +58,53 @@ if __name__ == '__main__':
         for clf, name in zip(classifiers, names):
             clf.fit(X_train, y_train_binary)
 
-        #print "Actual:   ", y_test_binary
-        #print "Predicted:", clf.predict(X_test)
-        #print "Probabilities:", clf.predict_proba(X_test)
             print name, "      :", clf.score(X_test, y_test_binary)
+
+def multilabel_classifier(X_train, y_train, X_test, y_test):
+
+    y_train_mlb = multilabel_binary_y(y_train)
+    y_test_mlb = multilabel_binary_y(y_test)
+
+    svc_ovr = Pipeline([('tfidf', TfidfVectorizer()),
+                        ('ovr-svc',OneVsRestClassifier(SVC(kernel='linear'),n_jobs=-2)),
+    ])
+
+    print "Fitting model"
+    svc_ovr.fit(X_train, y_train_mlb)
+    print "Done fitting, now predicting"
+    joblib.dump(svc_ovr, '../models/ovrmodel_ml.pkl')
+    y_pred = svc_ovr.predict(X_test)
+
+    joblib.dump(y_pred, '../models/pred_ml.pkl')
+
+def evaluate_multilabel(y_test, label_list):
+
+    y_test_mlb = multilabel_binary_y(y_test)
+    y_pred = joblib.load('../models/pred_ml.pkl')
+    print len(y_pred)
+    print len(y_test_mlb)
+
+    print "F1 score micro:", f1_score(y_test_mlb, y_pred, average='micro', labels=label_list)
+    print "F1 score weighted:", f1_score(y_test_mlb, y_pred, average='weighted', labels=label_list)
+    print "F1 score samples:", f1_score(y_test_mlb, y_pred, average='samples', labels=label_list)
+    print "F1 score:", f1_score(y_test_mlb, y_pred, average=None, labels=label_list)
+
+    print "Accuracy", accuracy_score(y_test_mlb, y_pred)
+
+    print classification_report(y_test_mlb, y_pred, target_names=label_list)
+
+    print zero_one_loss(y_test_mlb, y_pred)
+
+if __name__ == '__main__':
+
+    #Load data
+    label_list = dataset.load_labels()
+
+    X_train,y_train,filenames_train = dataset.load_train()
+    X_test,y_test,filenames_test = dataset.load_test()
+
+    print "Size of train set", len(X_train), ", Test set", len(X_test)
+
+    #classifier_per_label(X_train,y_train,X_test,y_test)
+    #multilabel_classifier(X_train,y_train,X_test,y_test)
+    evaluate_multilabel(y_test, label_list)
