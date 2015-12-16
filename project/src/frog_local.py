eab1e9935cf94cdfb6396055f244f91385efbd59
Guido <me@guido.io>
2015-12-16 02:30:59 +0100

    Resumable frog

--- project/src/frog_local.py
+++ project/src/frog_local.py
@@ -2,45 +2,50 @@ from __future__ import division
 import sys
 import time
 import frog
-
 import glob
+import util
 
 def duration_to_string(seconds):
     m, s = divmod(seconds, 60)
     h, m = divmod(m, 60)
     return "%d:%02d:%02d" % (h, m, s)
 
-if __name__ == '__main__':
-    INPUT_FOLDER = '../data/plaintext/'
-    OUTPUT_FOLDER = '../data/frogged/'
 
-    files = glob.glob(INPUT_FOLDER+'*.txt')
-    #print(files)
+def frog_process_files(files, verbose=False):
     seen = []
     start_time = time.time()
 
-    frog = frog.Frog(frog.FrogOptions(parser=False,mwu=False,ner=False,morph=False,chunking=False), "/etc/frog/frog.cfg")
+    frogger = frog.Frog(frog.FrogOptions(parser=False,mwu=False,ner=False,morph=False,chunking=False), "/etc/frog/frog.cfg")
 
     for i, filename in enumerate(files):
         with open(filename,'r') as in_file:
-            output = frog.process_raw(in_file.read())
-        print ('> PROCESSING', filename, str(len(seen))+'/'+str(len(files)))
+            output = frogger.process_raw(in_file.read())
 
-        seen.append(filename)
+        if verbose:
+            print ('> PROCESSING', filename, str(len(seen))+'/'+str(len(files)))
 
-        #Timings (estimation of time remaining)
-        runtime = time.time() - start_time
-        per_document_time = runtime/len(seen)
-        remaining_time = (len(files)-len(seen))*per_document_time
-        total_time = remaining_time+runtime
+            seen.append(filename)
 
+            #Timings (estimation of time remaining)
+            runtime = time.time() - start_time
+            per_document_time = runtime/len(seen)
+            remaining_time = (len(files)-len(seen))*per_document_time
+            total_time = remaining_time+runtime
 
-        print ("RUNTIME", duration_to_string(runtime),
-         "("+duration_to_string(per_document_time)+")",
-          'REMAINING', duration_to_string(remaining_time),
-           'TOTAL', duration_to_string(total_time))
+            print ("RUNTIME", duration_to_string(runtime),
+             "("+duration_to_string(per_document_time)+")",
+              'REMAINING', duration_to_string(remaining_time),
+               'TOTAL', duration_to_string(total_time))
 
-        frogged_filename = filename.split('/')[-1].split('.txt')[0]
+        frogged_filename = util.filename_without_extension(filename, '.txt')
+        #frogged_filename = filename.split('/')[-1].split('.txt')[0]
 
         with open(OUTPUT_FOLDER+frogged_filename+'.frog.out', 'w') as f:
             f.write(output)
+
+if __name__ == '__main__':
+    INPUT_FOLDER = '../data/plaintext/'
+    OUTPUT_FOLDER = '../data/frogged_new/'
+
+    files = util.todo_filepaths(INPUT_FOLDER, '.txt', OUTPUT_FOLDER, '.frog.out')
+    frog_process_files(files)
--- project/src/util.py
+++ project/src/util.py
@@ -6,7 +6,7 @@ CPU_COUNT = multiprocessing.cpu_count()
 
 
 def filename_without_extension(filepath, extension='.xml'):
-    return filepath.split('\\')[-1][:-1*len(extension)]
+    return filepath.split(os.sep)[-1][:-1*len(extension)]
 
 def chunks(l, n):
     n = max(1, n)
@@ -21,8 +21,8 @@ def todo_filepaths(in_folder, in_extension, out_folder=None, out_extension=None,
         out_filenames = []
 
     #Just in case
-    in_filenames = map(os.path.normpath, in_filenames)
-    out_filenames = map(os.path.normpath, out_filenames)
+    in_filenames = list(map(os.path.normpath, in_filenames))
+    out_filenames = list(map(os.path.normpath, out_filenames))
     blacklist = map(os.path.normpath, blacklist)
 
     s = set(blacklist)
@@ -34,12 +34,12 @@ def todo_filepaths(in_folder, in_extension, out_folder=None, out_extension=None,
     #Dictionary from filename without extension to full input path
     in_dict = {file_id: file_path for file_id, file_path in zip(in_ids, in_filenames)}
 
-    print "input: {0} ({1}) {2}".format(in_folder, in_extension, len(in_filenames))
-    print "output: {0} ({1}) {2}".format(out_folder, out_extension, len(out_filenames))
+    print ("input: {0} ({1}) {2}".format(in_folder, in_extension, len(in_filenames)))
+    print ("output: {0} ({1}) {2}".format(out_folder, out_extension, len(out_filenames)))
 
     done = set(out_ids)
     todo_ids = [x for x in in_ids if x not in done]
 
-    print "todo: {0}\n\n".format(len(todo_ids))
+    print ("todo: {0}\n\n".format(len(todo_ids)))
 
     return [in_dict[todo_id] for todo_id in todo_ids]
